e6848c0633c046219ed5a2db1000c6c71708ee0f
thifaya <tmollootimile@skhomotech.co.za>
2021-02-21 21:41:54 +0200

    ng-bootstrap library updated

--- src/app/views/pages/_elections/elections.module.ts
+++ src/app/views/pages/_elections/elections.module.ts
@@ -9,6 +9,20 @@ import { ElectionsComponent } from './elections.component'
 import { ElectionTableComponent } from './elections-table/election-table.component';
 import { ElectionsOverviewComponent } from './elections-overview/elections-overview.component';
 
+import { CodePreviewModule } from '../../partials/code-preview/code-preview.module';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { FormsModule } from '@angular/forms';
+
+import { FeahterIconModule } from '../../../core/feather-icon/feather-icon.module';
+
+import { PerfectScrollbarModule } from 'ngx-perfect-scrollbar';
+import { PERFECT_SCROLLBAR_CONFIG } from 'ngx-perfect-scrollbar';
+import { PerfectScrollbarConfigInterface } from 'ngx-perfect-scrollbar';
+
+const DEFAULT_PERFECT_SCROLLBAR_CONFIG: PerfectScrollbarConfigInterface = {
+  suppressScrollX: true
+};
+
 const routes: Routes = [
   {
     path: '',
@@ -34,7 +48,18 @@ const routes: Routes = [
   imports: [
     CommonModule,
     RouterModule.forChild(routes),
-    NgxDatatableModule
+    NgxDatatableModule,
+    CodePreviewModule,
+    NgbModule,
+    PerfectScrollbarModule,
+    FormsModule,
+    FeahterIconModule
+  ],
+  providers: [
+    {
+      provide: PERFECT_SCROLLBAR_CONFIG,
+      useValue: DEFAULT_PERFECT_SCROLLBAR_CONFIG
+    }
   ]
 })
 export class ElectionsModule { }
