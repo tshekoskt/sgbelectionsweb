9a608e6e5a8fb19f458921ee8e8eafa550adb873
thifaya <tmollootimile@skhomotech.co.za>
2021-02-24 01:20:05 +0200

    created the election nomination page

--- src/app/views/pages/_election-nomination/nominations/nominations.component.ts
+++ src/app/views/pages/_election-nomination/nominations/nominations.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef, Inject, Renderer2 } from '@angular/core';
+import { DOCUMENT } from '@angular/common';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-nominations',
@@ -7,9 +9,74 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NominationsComponent implements OnInit {
 
-  constructor() { }
+
+  parents: any[] = [
+    {
+      name: "Timothy Mabina",
+      nominated: "True"
+    },
+    {
+      name: "Tshepo Mmutlwane",
+      nominated: "True"
+    },
+    {
+      name: "Karabo Letsoko",
+      nominated: "True"
+    },
+    {
+      name: "Duduzile Nkomo",
+      nominated: "True"
+    },
+    {
+      name: "Grace Matimela",
+      nominated: "True"
+    },
+    {
+      name: "Tefo Mbina",
+      nominated: "True"
+    },
+    {
+      name: "Tunelo Mmutlwa",
+      nominated: "True"
+    },
+    {
+      name: "Karabo Nnete",
+      nominated: "True"
+    },
+    {
+      name: "Dudu Nkgomo",
+      nominated: "True"
+    },
+    {
+      name: "Goitsi Maime",
+      nominated: "True"
+    }
+  ];
+
+
+  constructor(
+    @Inject(DOCUMENT) private document: Document,
+    private renderer: Renderer2,
+    private router: Router
+  ) { }
 
   ngOnInit(): void {
+  }
+
+
+  toggleSidebar(e) {
+    e.preventDefault();
+    this.document.body.classList.toggle('sidebar-open');
+  }
+
+
+  onLogout(e) {
+    e.preventDefault();
+    localStorage.removeItem('isLoggedin');
+
+    if (!localStorage.getItem('isLoggedin')) {
+      this.router.navigate(['/auth/login']);
+    }
   }
 
 }
