7751094ca2899af2552d55936f5eeafba826a47b
thifaya <tmollootimile@skhomotech.co.za>
2021-02-24 23:42:17 +0200

    created parent auth services and fixed meeting api

--- src/app/views/pages/_Meetings/meetings-table/meetings-table.component.ts
+++ src/app/views/pages/_Meetings/meetings-table/meetings-table.component.ts
@@ -9,101 +9,26 @@ import { ElectionsService } from '../../_SERVICE/elections.service';
 })
 export class MeetingsTableComponent implements OnInit {
 
-  meetings = [];
-  meeting = [];
-  //  [
-  //   {
-  //     title: "Meeting 09",
-  //     startDate: "02/10/2021",
-  //     startTime: "11:00",
-  //     scheduledBy: "Jonathan Masilela"
-  //   },
-  //   {
-  //     title: "Meeting 10",
-  //     startDate: "02/01/2021",
-  //     startTime: "10:00",
-  //     scheduledBy: "Mary Nkuna"
-  //   },
-  //   {
-  //     title: "Meeting 08",
-  //     startDate: "10/10/2021",
-  //     startTime: "11:00",
-  //     scheduledBy: "Deborah Zule"
-  //   },
-  //   {
-  //     title: "Meeting 03",
-  //     startDate: "02/06/2021",
-  //     startTime: "07:00",
-  //     scheduledBy: "Jonathan Masilela"
-  //   },
-  //   {
-  //     title: "Meeting 09",
-  //     startDate: "27/10/2021",
-  //     startTime: "05:00",
-  //     scheduledBy: "Deborah Zule"
-  //   },
-  //   {
-  //     title: "Meeting 04",
-  //     startDate: "02/10/2021",
-  //     startTime: "11:00",
-  //     scheduledBy: "Mildred Masilo"
-  //   },
-  //   {
-  //     title: "Meeting 02",
-  //     startDate: "02/04/2021",
-  //     startTime: "11:00",
-  //     scheduledBy: "Mary Nkuna"
-  //   },
-  //   {
-  //     title: "Meeting 07",
-  //     startDate: "02/10/2021",
-  //     startTime: "11:00",
-  //     scheduledBy: "Jonathan Masilela"
-  //   },
-  //   {
-  //     title: "Meeting 01",
-  //     startDate: "04/04/2021",
-  //     startTime: "08:30",
-  //     scheduledBy: "Deborah Zule"
-  //   },
-  //   {
-  //     title: "Meeting 07",
-  //     startDate: "02/10/2021",
-  //     startTime: "11:30",
-  //     scheduledBy: "Kgothatso Moremi"
-  //   },
-  //   {
-  //     title: "Meeting 05",
-  //     startDate: "02/10/2021",
-  //     startTime: "08:00",
-  //     scheduledBy: "Kgothatso Moremi"
-  //   },
-  // ]
-  // title: String,
-  // startDate: String,
-  // startTime: String,
-  // endDate: String,
-  // endTime: String,
-  // venue: String,
-  // description: String,
-  // minutes: String,
-  // scheduledBy: String
+  meetings;
+  meeting;
+
 
   constructor(private _meetingService: ElectionsService) { }
 
   ngOnInit(): void {
     const dataTable = new DataTable("#dataTableExample");
-    this._meetingService.getAllMeetings().subscribe(res =>{
+    this._meetingService.getAllMeetings().subscribe(res => {
 
       this.meetings = res;
+      console.log(this.meetings);
 
-      for(var x=0;x<this.meetings.length;x++){
+      for (var x = 0; x < this.meetings.length; x++) {
         this.meeting = [
           this.meetings[x].title,
           this.meetings[x].startDate,
           this.meetings[x].startTime,
           this.meetings[x].scheduledBy,
-          "<a><i class='mdi mdi-eye text-dark' (click)='view("+x+")'></i></a>&nbsp;&nbsp;<a><i class='mdi mdi-pencil text-dark' (click)='edit("+x+")'></i></a>"
+          "<a><i class='mdi mdi-eye text-dark' (click)='view(0)'></i></a>&nbsp;&nbsp;&nbsp;<a><i class='mdi mdi-pencil text-dark' (click)='edit(0)'></i></a>"
         ]
       }
 
@@ -121,7 +46,6 @@ export class MeetingsTableComponent implements OnInit {
       }
       dataTable.insert(newData);
 
-      console.log(this.meeting);
 
 
     });
--- src/app/views/pages/_SERVICE/elections.service.ts
+++ src/app/views/pages/_SERVICE/elections.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { IMeetings } from './Interface/meetings';
-import { Observable } from 'rxjs';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+// import { IMeetings } from './Interface/meetings';
+// import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -10,17 +10,65 @@ export class ElectionsService {
 
   constructor(private http: HttpClient) { }
 
-  // API
-  meetings_url = "https://sgbserviceapi.azurewebsites.net/api/Meeting/MeetingList"
+  Header: HttpHeaders = new HttpHeaders({
+    'Content-Type': 'application/json',
+    'responseType': 'json',
+    'Access-Control-Allow-Origin': '*',
+    'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, PATCH, OPTIONS',
+    'Access-Control-Allow-Headers': 'X-Requested-With, content-type, Authorization'
+  });
 
- // API Requests
+  // URL
+  meetings_url = "https://sgbserviceapi.azurewebsites.net/api/Meeting/MeetingList";
+  base_url = "https://sgbserviceapi.azurewebsites.net";
 
- getAllMeetings(): Observable<IMeetings[]>{
-   return this.http.get<IMeetings[]>(this.meetings_url);
- }
 
-// getAllMeetings() {
-//   return this.http.get(this.meetings_url);
-// }
+
+  ////////////// MEETING service ////////////////////////
+  getAllMeetings() {
+    return this.http.get(this.meetings_url);
+  }
+
+
+  ////////////// AUTH service ////////////////////////
+  authenticateParentIDNmber(jsonData) {
+    return this.http.post(this.base_url + "/api/Login/AuthenticateParent", jsonData, { headers: this.Header })
+  }
+
+  authParentOTP(phonenumber, otp) {
+    return this.http.post(this.base_url + "/AuthenticateOTP?MobileNumber=" + phonenumber + "&OTP=" + otp, { headers: this.Header })
+  }
+
+  sendOTP(IDnumber) {
+    return this.http.post(this.base_url + "/SendOTP?IDNumber=" + IDnumber, { headers: this.Header })
+  }
+
+  resendOTP(id) {
+    return this.http.get(this.base_url + "/ResendOTP?Id=" + id, { headers: this.Header })
+  }
+
+
+
+  ////////////// PARENT service ////////////////////////
+
+  getParentInfo(id) {
+    return this.http.get(this.base_url + "/GetParentInfo?IDNumber=" + id, { headers: this.Header })
+  }
+
+  getSchoolByParentId(id) {
+    return this.http.get(this.base_url + "/GetChildrenSchoolByParentId?ParentId=" + id, { headers: this.Header })
+  }
+
+  getLearnersByParentId(id, emisCode) {
+    return this.http.get(this.base_url + "/GetLeanersByParent?ParentId=" + id + "&EmisCode=" + emisCode, { headers: this.Header })
+  }
+
+
+
+  /*
+  https://sgbserviceapi.azurewebsites.net/GetParentInfo?IDNumber=9401095569086
+  /GetChildrenSchoolByParentId?ParentId=<integer>
+  /GetLeanersByParent?ParentId=<string>&EmisCode=<string>
+  */
 
 }
