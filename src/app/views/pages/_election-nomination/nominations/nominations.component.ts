0a69c426e5913b0c4c796a482fcdfce591231977
Tsheko Mashego <support@pulego.co.za>
2021-02-27 09:36:12 +0200

    preelection screen

--- src/app/views/pages/_election-nomination/nominations/nominations.component.ts
+++ src/app/views/pages/_election-nomination/nominations/nominations.component.ts
@@ -1,19 +1,20 @@
 import {
   Component,
-  OnInit,
-  ViewChild,
-  ElementRef,
-  Inject,
-  Renderer2
-} from '@angular/core';
-import {
-  DOCUMENT
-} from '@angular/common';
+  OnInit} from '@angular/core';
+import * as _ from 'lodash';
 import {
   Router
 } from '@angular/router';
 import Swal from 'sweetalert2';
-import { ElectionsService } from '../../_SERVICE/elections.service';
+import {
+  ElectionsService
+} from '../../_SERVICE/elections.service';
+import {
+  FormArray,
+  FormBuilder,
+  FormControl,
+  FormGroup
+} from '@angular/forms';
 
 @Component({
   selector: 'app-nominations',
@@ -22,52 +23,77 @@ import { ElectionsService } from '../../_SERVICE/elections.service';
 })
 export class NominationsComponent implements OnInit {
 
+  form: FormGroup;
 
-  nominatedparents: any[] = [];
+  selectedNominee: any = {};
 
-  selectedNominee;
+  selectedSeconder: any = {};
 
-  nominations;
-  seconded;
+  blockB;
 
+  blockA: any[] = [{
+      firstName: "Timothy Mabina",
+      lastName: "Mabina"
+    },
+    {
+      firstName: "Tshepo Mmutlwane",
+      lastName: "TrMmutlwaneue"
+    },
+    {
+      firstName: "Karabo Letsoko",
+      lastName: "Letsoko"
+    }
+  ];
 
   constructor(
-    @Inject(DOCUMENT) private document: Document,
-    private renderer: Renderer2,
+    private formBuilder: FormBuilder,
     private router: Router,
     private _nominationService: ElectionsService
   ) {}
 
   ngOnInit(): void {
 
-    this._nominationService.getProposedNominationsByEmisCode(700400139,5352).subscribe(res => {
-
-      this.nominations = res;
-    console.log(this.nominations);
+    this.form = this.formBuilder.group({
+      blockAList: new FormArray([]),
+      blockBList: new FormArray([])
     });
 
-    this._nominationService[] = [
-      {
-        firstName: "Timothy Mabina",
-        lastName: "Mabina"
-      },
-      {
-        firstName: "Tshepo Mmutlwane",
-        lastName: "TrMmutlwaneue"
-      },
-      {
-        firstName: "Karabo Letsoko",
-        lastName: "Letsoko"
-      }
-    ]
+    //console.log(this.nominations);
+    // this._nominationService.getProposedNominationsByEmisCode(700400139,5352).subscribe(res => {
 
-    this._nominationService.getNominationsToBeSecondedByEmisCode(700400139,5352).subscribe(sec => {
+    //   this.nominations = res;
+    // console.log(this.nominations);
+    // });
 
-      this.seconded = sec;
+    this.blockA.forEach(() => {
+
+      this.blockAFormArray.push(new FormControl(false));
+
+    });
+
+
+    this._nominationService.getNominationsToBeSecondedByEmisCode(700400139, 5352).subscribe(res => {
+
+      this.blockB = res;
+
+      this.blockB.forEach(() => {
+
+        this.blockBFormArray.push(new FormControl(false));
+
+      });
 
     });
   }
 
+  get blockAFormArray() {
+    console.log(this.form.controls.blockAList);
+    return this.form.controls.blockAList as FormArray;
+
+  }
+
+  get blockBFormArray() {
+    return this.form.controls.blockBList as FormArray;
+  }
 
   toggleSidebar(e) {
     e.preventDefault();
@@ -83,55 +109,108 @@ export class NominationsComponent implements OnInit {
     }
   }
 
-  nominate() {
-    this.nominatedparents.push(this.selectedNominee);
-    const index: number = this.nominations.indexOf(this.selectedNominee);
-    if (index !== -1) {
-      this.nominations.splice(index, 1);
+  nominate(e) {
+
+    if (e.target.checked) {
+      
+      this.selectedNominee.canBeSelected = true;
+      this.blockBFormArray.push(new FormControl(false));
+      
+      
+      this.blockB.push(this.selectedNominee);
+      this.blockA.splice(e.target.value, 1);
     }
-    console.log(this.selectedNominee);
+
   }
 
-  confirmBox(name) {
+  confirmBox(e) {
 
-    if (this.seconded.length == 0) {
-      Swal.fire({
-        title: 'Are you sure you want to nominate ' + name + '?',
-        text: 'You will not be able to nominate anymore!',
-        icon: 'warning',
-        showCancelButton: true,
-        confirmButtonText: 'Yes, Nominate!',
-        cancelButtonText: 'No, Do not nominate'
-      }).then((result) => {
-        if (result.value) {
+    if(_.isEmpty(this.selectedNominee)===false)
+    {
+        Swal.fire(
+          'Already Nominated',
+          'You have already nominated. To reset your selection, please click RESET button',
+          'error'
+        );
 
-          this.nominate();
+        return;
+    }
 
-          Swal.fire(
-            'Confirmation!',
-            name + ' has been nominated to be elected.',
-            'success'
-          )
-        } else if (result.dismiss === Swal.DismissReason.cancel) {
-          Swal.fire(
-            'Cancelled',
-            'Your nomination was cancelled',
-            'error'
-          )
-        }
-      })
-    } else
-      //this.selectedNominee = {};
-      Swal.fire(
-        'Already Nominated',
-        'Your have already nominated',
-        'error'
-      )
+    this.selectedNominee = this.blockA[e.target.value];
+    const selectedNominatedParentName: string =  this.selectedNominee?.firstName + ' ' + this.selectedNominee?.lastName;
+
+    Swal.fire({
+      title: 'Are you sure you want to nominate ' + selectedNominatedParentName + '?',
+      text: 'You will not be able to nominate anymore!',
+      icon: 'question',
+      showCancelButton: true,
+      confirmButtonText: 'Yes, Nominate!',
+      cancelButtonText: 'No, Do not nominate'
+    }).then((result) => {
+      if (result.value) {
+
+        this.nominate(e);
+
+        Swal.fire(
+          'Confirmation!',
+          selectedNominatedParentName + ' has been nominated to be elected.',
+          'success'
+        )
+      } else if (result.dismiss === Swal.DismissReason.cancel) {
+        Swal.fire(
+          'Cancelled',
+          'Your nomination was cancelled',
+          'error'
+        )
+      }
+    })
+
   }
 
-  markAsParentCompletedNomination(emisCode,parentId)
-  {
+  confirmSecondBox(e) {
 
+    this.selectedSeconder = this.blockB[e.target.value];
+    const selectedSecondedParentName: string =  this.selectedSeconder?.firstName + ' ' + this.selectedSeconder?.lastName;
+
+    Swal.fire({
+      title: 'Are you sure you want to second ' + selectedSecondedParentName + '?',
+      text: 'You will not be able to second anymore!',
+      icon: 'question',
+      showCancelButton: true,
+      confirmButtonText: 'Yes, Seconder!',
+      cancelButtonText: 'No'
+    }).then((result) => {
+      if (result.value) {
+
+        Swal.fire(
+          'Confirmation!',
+          selectedSecondedParentName + ' has been seconded.',
+          'success'
+        )
+      } else if (result.dismiss === Swal.DismissReason.cancel) {
+        Swal.fire(
+          'Cancelled',
+          'Your seconding was cancelled',
+          'error'
+        )
+      }
+    })
+
+  }
+
+  removeNomination(e) {
+      this.selectedNominee.canBeSelected = false;
+      this.blockAFormArray.push(new FormControl(false));
+      console.log(this.selectedNominee);
+      this.blockA.push(this.selectedNominee);
+      this.blockB.splice(e.target.value, 1);
+  }
+
+  saveChanges() {
+    //emisCode,parentId
+    //markAsParentCompletedNomination
+
+    this.router.navigate(['/parentlanding']);
   }
 
 }
